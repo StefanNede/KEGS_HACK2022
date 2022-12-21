6d49da770e0385d57cc8266d22b80580b0fb09ff
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-12-21 21:15:42 +0000

    added buggy reindeer AI

--- actors/Reindeer.gd
+++ actors/Reindeer.gd
@@ -3,10 +3,13 @@ extends KinematicBody2D
 class_name Reindeer
 
 onready var health_stat: Health = $Health
+onready var ai: Reindeer_AI = $AI
 
 var starting_health := 150
+var speed := 400
 
 func _ready() -> void:
+	ai.initialise(self)
 	health_stat.health = starting_health
 
 func handle_hit() -> void:
@@ -14,3 +17,11 @@ func handle_hit() -> void:
 	if health_stat.health <= 0:
 		queue_free()
 	# print("enemy health now at: " + str(health_stat.health))
+
+func rotate_towards(location: Vector2) -> void:
+	# rotation is in radians
+	# lerp makes it look more natural when the AI turns around - it doesn't just snap -> takes in (current rotation, final rotation, weight)
+	rotation = lerp(rotation, global_position.direction_to(location).angle(), 0.1)
+
+func velocity_towards(location: Vector2) -> Vector2:
+	return global_position.direction_to(location) * speed
---
+++ actors/ReindeerAI.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+class_name Reindeer_AI
+
+onready var timer = $Timer
+onready var player = get_node("../../Player")
+
+var actor: KinematicBody2D = null
+var actor_velocity: Vector2 = Vector2.ZERO
+var target_position: Vector2
+var angle_to_target: float
+var target_position_reached: bool
+
+func _ready() -> void:
+	timer.start()
+
+func initialise(actor) -> void:
+	self.actor = actor
+
+func _process(delta: float) -> void:
+	if abs(actor.global_position.x - target_position.x) < 1 and abs(actor.global_position.y - target_position.y) < 1:
+		# also need to check if reindeer has collided with anything in which case we set target_position_reached to be true
+		target_position_reached = true
+	
+	if target_position_reached:
+		# print("target position reached")
+		timer.start()
+		
+	elif player != null and not target_position_reached:
+		# print("moving to target position")
+		actor.rotate_towards(target_position)
+		if abs(actor.rotation - angle_to_target) < 0.1:
+			# charge at the player
+			actor.move_and_slide(actor_velocity)
+
+func _on_Timer_timeout():
+	if player != null:
+		print("new target position set")
+		target_position = player.global_position
+		print(actor.global_position, target_position)
+		angle_to_target = actor.global_position.direction_to(target_position).angle()
+		actor_velocity = actor.velocity_towards(target_position)
